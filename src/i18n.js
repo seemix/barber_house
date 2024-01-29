131c34c90a65f529cb1c9d1182dad74c639d37ab
seemix <91602641+seemix@users.noreply.github.com>
2024-01-29 22:37:52 +0200

    animations 1

--- src/i18n.js
+++ src/i18n.js
@@ -6,8 +6,8 @@ import { initReactI18next } from 'react-i18next'
 i18n
     .use(Backend)
     .use(LanguageDetector).use(initReactI18next).init({
-     fallbackLng: 'dk',
-    debug: true,
+     fallbackLng: 'en',
+    debug: false,
      interpolation: {
         escapeValue: false
     },
