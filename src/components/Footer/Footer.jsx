cb249f2dfae90de7578f82f8ccc778f18e6d56e3
seemix <91602641+seemix@users.noreply.github.com>
2024-02-24 12:57:46 +0200

    footer modify

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,36 +1,39 @@
 import { MdOutlinePhoneAndroid } from 'react-icons/md';
 import { useTranslation } from 'react-i18next';
 
-import logo from './bottom_logo.webp';
+import logo from './bottom_logo1.webp';
 import css from './Footer.module.css';
 import { BookingButton } from '../index.js';
 
 const Footer = () => {
     const { t } = useTranslation();
     return (
-        <div className={css.footer_wrapper}>
-            <div className={css.footer_content}>
-                <div className={css.logo}>
-                    <img src={logo} alt={'footer_logo'}/>
-                </div>
-                <div>
-                    <h4>Booking</h4>
-                    <BookingButton/>
-                </div>
-                <div>
-                    <h4>{t('Address')}</h4>
-                    <p>
-                        Istedgade 28, 8700 Horsens
-                    </p>
-                    <MdOutlinePhoneAndroid size={'1.1em'}/> +(45) 52 64 42 96
-                </div>
-                <div>
-                    <h4>{t('Work hours')}</h4>
-                    <p>{t('Mon-Sat')} 10:00-18:00</p>
-                    <p>{t('Sun: closed')}</p>
+        <footer>
+            <div className={css.footer_wrapper}>
+                <div className={css.footer_content}>
+                    <div className={css.logo}>
+                        <img src={logo} alt={'footer_logo'}/>
+                    </div>
+                    <div>
+                        <h4>Booking</h4>
+                        <BookingButton/>
+                    </div>
+                    <div>
+                        <h4>{t('Address')}</h4>
+                        <p>
+                            Istedgade 28, 8700 Horsens
+                        </p>
+                        <MdOutlinePhoneAndroid size={'1.1em'}/> +(45) 52 64 42 96
+                    </div>
+                    <div>
+                        <h4>{t('Work hours')}</h4>
+                        <p>{t('Mon-Sat')} 10:00-18:00</p>
+                        <p>{t('Sun: closed')}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className={css.under_footer}>Copyright © Barbers House 2024</div>
+        </footer>
     );
 };
 
