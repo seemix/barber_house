d5021ee243c4ee79840f51805690cd46aacbb5e1
seemix <91602641+seemix@users.noreply.github.com>
2025-04-16 14:30:07 +0300

    faq start

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,35 +1,97 @@
+import { useTranslation } from 'react-i18next';
+
 import { SectionCaption } from '../index.js';
-import './Faq.css';
+import css from './Faq.module.css';
 
 const Faq = () => {
+    const { t } = useTranslation();
     return (
         <section id={'faq'}>
             <SectionCaption caption={'FAQ'}/>
-
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <div className={'c'}>
+            <div className={css.faq_wrapper}>
+                <div className={css.faq_content}>
+                    <div className={css.c}>
                         <input type="checkbox" id="faq-1"/>
-                        <h3><label htmlFor="faq-1">What Is This ?</label></h3>
-                        <div className="p">
-                            <p>This a very very simple accordion.</p>
+                        <h3><label htmlFor="faq-1">{t('fq1')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa1')}</p>
                         </div>
                     </div>
-                    <div className="c">
+                    <div className={css.c}>
                         <input type="checkbox" id="faq-2"/>
-                        <h3><label htmlFor="faq-2">With Pure Css ?</label></h3>
-                        <div className="p">
-                            <p>Yes with pure CSS and HTML.</p>
+                        <h3><label htmlFor="faq-2">{t('fq2')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa2')}</p>
                         </div>
                     </div>
-                    <div className="c">
+                    <div className={css.c}>
                         <input type="checkbox" id="faq-3"/>
-                        <h3><label htmlFor="faq-3">Where did you get inpiration ?</label></h3>
-                        <div className="p">
-                            <p>I was inpired by an article on css-tricks. <a
-                                href="https://css-tricks.com/the-checkbox-hack/">link
-                                to article</a>
-                            </p>
+                        <h3><label htmlFor="faq-3">{t('fq3')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa3')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-4"/>
+                        <h3><label htmlFor="faq-4">{t('fq4')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa4')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-5"/>
+                        <h3><label htmlFor="faq-5">{t('fq5')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa5')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-6"/>
+                        <h3><label htmlFor="faq-6">{t('fq6')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa6')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-7"/>
+                        <h3><label htmlFor="faq-7">{t('fq7')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa7')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-8"/>
+                        <h3><label htmlFor="faq-8">{t('fq8')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa8')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-9"/>
+                        <h3><label htmlFor="faq-9">{t('fq9')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa9')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-10"/>
+                        <h3><label htmlFor="faq-10">{t('fq10')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa10')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-11"/>
+                        <h3><label htmlFor="faq-11">{t('fq11')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa11')} </p>
+                        </div>
+                    </div>
+                    <div className={css.c}>
+                        <input type="checkbox" id="faq-12"/>
+                        <h3><label htmlFor="faq-12">{t('fq12')}</label></h3>
+                        <div className={css.p}>
+                            <p>{t('fa12')} </p>
                         </div>
                     </div>
                 </div>
--- src/components/Slider/animations.js
+++ src/components/Slider/animations.js
@@ -14,11 +14,13 @@ export const buttonAnimation = {
 export const headerAnimation = {
     hidden: {
         y: 30,
-        opacity: 0
+        opacity: 0,
+        filter: 'blur(10px)'
     },
     visible: custom => ({
         x: 0,
         opacity: 1,
-        transition: { delay: custom * 0.3, duration: 0.4, ease: 'easeInOut'  }
+        filter: 'blur(0)',
+        transition: { delay: custom * .3, duration: .4, ease: 'easeInOut'  }
     })
 };
